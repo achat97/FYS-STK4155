009351d46de64c5fcf849cd852876af0ff7d4e3c
Achraf Atifi <achraf.atifi@outlook.com>
2022-10-24 10:37:42 +0200

    Added report, figures and script

--- Project1/Src/fig211.py
+++ Project1/Src/fig211.py
@@ -1,70 +1,40 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from sklearn.metrics import mean_squared_error, r2_score
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import numpy as np
-from random import random, seed
-from sklearn.model_selection import train_test_split
-import sklearn.linear_model as skl
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from functions import *
 
 
-
 # Make data.
-N = 50
-maxdegree = 30
-x = np.linspace(0, 1, N)
-y = np.linspace(0, 1, N)
-x, y = np.meshgrid(x,y)
-np.random.seed(2018)
-noise = np.random.normal(0,1,(N,N))
+def figHastie(N,z_tuning,maxdegree,plot=True):
+    x,y,z = Data(N,z_tuning)
+    z = z.ravel()
 
+    MSE_test = np.zeros(maxdegree)
+    MSE_train = np.zeros(maxdegree)
+    polydegree = np.zeros(maxdegree)
 
-z = FrankeFunction(x,y)+0.2*noise
-z = z.ravel()
+    X = create_X(x, y, maxdegree)
+    X_train, X_test, z_train, z_test = train_test_split(X,z,test_size=0.2)
 
-beta_parameters = np.zeros(maxdegree,dtype=object)
-MSE_train = np.zeros(maxdegree)
-R2_train = np.zeros(maxdegree)
-MSE_test= np.zeros(maxdegree)
-R2_test = np.zeros(maxdegree)
+    print("Calculating error for  degree..")
+    for degree in range(1,maxdegree+1):
+        print(degree)
 
-X = create_X(x, y, maxdegree)
-X_train, X_test, z_train, z_test = train_test_split(X,z,test_size=0.2)
-X_train_scaled_all,X_test_scaled_all,z_train_scaled,z_test_scaled = mean_scaler(X_train,X_test,z_train,z_test)
+        X_train_split,X_test_split = features(X_train,degree),features(X_test,degree)
+        X_train_scaled,X_test_scaled,z_train_scaled,z_test_scaled = centering(X_train_split,X_test_split,z_train,z_test)
 
+        beta = OLS(X_train_scaled,z_train_scaled)
+        zpredict_test = X_test_scaled@beta
+        zpredict_train = X_train_scaled@beta
 
-for degree in range(1,maxdegree+1):
-    print(f"Degree:{degree}/{maxdegree}")
-    X_train_scaled,X_test_scaled = features(X_train_scaled_all,X_test_scaled_all,degree)
-    beta = OLS(X_train_scaled,z_train_scaled)
-    zpredict_test = X_test_scaled@beta
-    zpredict_train = X_train_scaled@beta
+        MSE_train[degree-1] = MSE(z_train_scaled,zpredict_train)
+        MSE_test[degree-1]= MSE(z_test_scaled,zpredict_test)
+        polydegree[degree-1] = degree
 
-    MSE_train[degree-1] = MSE(z_train_scaled,zpredict_train)
-    R2_train[degree-1] = R2(z_train_scaled,zpredict_train)
-    MSE_test[degree-1]= MSE(z_test_scaled,zpredict_test)
-    R2_test[degree-1] = R2(z_test_scaled,zpredict_test)
+    if plot:
+        plt.style.use('seaborn-v0_8')
+        plt.plot(polydegree,MSE_train,color="midnightblue",label="Training")
+        plt.plot(polydegree,MSE_test,'--',color="brown",label="Test")
+        plt.xlabel("Complexity")
+        plt.ylabel("MSE")
+        plt.xticks(polydegree)
+        plt.legend()
 
-
-plt.subplot(121)
-plt.plot(np.arange(1,maxdegree+1),MSE_train,color="r",label="Training")
-plt.plot(np.arange(1,maxdegree+1),MSE_test,'--k',label="Test")
-plt.xlabel("Complexity")
-plt.ylabel("MSE")
-plt.title("Mean Square Error")
-plt.legend()
-
-
-plt.subplot(122)
-plt.plot(np.arange(1,maxdegree+1),R2_train,color="r",label="Training")
-plt.plot(np.arange(1,maxdegree+1),R2_test,'--k',label="Test")
-plt.xlabel("Complexity")
-plt.ylabel(f"$R^2$")
-plt.title(f"$R^2-Score$")
-
-
-plt.legend()
-plt.show()
+        plt.show()
--- Project1/Src/functions.py
+++ Project1/Src/functions.py
@@ -4,10 +4,28 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from sklearn.utils import resample
+from imageio import imread
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+import sklearn.linear_model as skl
+from sklearn.model_selection import train_test_split,KFold
 from random import random, seed
+from sklearn import linear_model
+
+
 
 
 def FrankeFunction(x,y):
+    """
+    Calculates the function values of the Franke function given x and y
+
+    Args:
+        x (ndarray): x-values
+        y (ndarray): y-values
+
+    Returns:
+        f(x,y) (ndarray): function values
+    """
+
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
@@ -15,14 +33,87 @@ def FrankeFunction(x,y):
     return term1 + term2 + term3 + term4
 
 
+
+
+def Data(N,z_tuning):
+
+    """
+    Generates x,y and z values
+
+    Args:
+    N (int): number of datapoints
+    z_tuning (float): tuning parameter for noise
+
+    Returns:
+
+        z (ndarray): Franke function values over meshgrid
+    """
+
+    np.random.seed(2018)
+
+    x = np.linspace(0, 1, N)
+    y = np.linspace(0, 1, N)
+
+    x, y = np.meshgrid(x,y)
+    z = FrankeFunction(x, y) + z_tuning*np.random.randn(N, N)
+
+    return x,y,z
+
+
+
+
+def Data_terrain(tifFile,N,Nx,Ny):
+    """
+    Takes terrain data and return a cropped version of it in addition to producing random x and y data
+
+    Args:
+    tifFile (tif): tif-file with terrain data
+    N (int): size of cropped version
+    Nx (int): starting point (x-direction) in the terrain data for cropped version
+    Ny (int): starting point (y-direction) in the terrain data for cropped version
+
+    Returns:
+    terrainData (ndarray): full terrain data
+    x (ndarray): x-values over meshgrid
+    y (ndarray): y-values over meshgrid
+    z (ndarray): cropped terrain data
+    """
+
+    terrainData = np.array(imread(tifFile))
+    size = N
+    ylen, xlen = np.shape(terrainData)
+    xregion = Nx
+    yregion = Ny
+    z = terrainData[xregion : xregion + size, yregion : yregion + size]
+
+    np.random.seed(2018)
+    x = np.sort(np.random.rand(size))
+    y = np.sort(np.random.rand(size))
+    x, y = np.meshgrid(x, y)
+
+    return terrainData,x,y,z
+
+
 def create_X(x, y, n):
+    """
+    Constructs design matrix
+
+    Args:
+        x (ndarray): x-values over meshgrid
+        y (ndarray): x-values over meshgrid
+        n (int): degree
+
+    Returns:
+        X (ndarray): design matrix
+
+    """
 
     if len(x.shape)>1:
         x = np.ravel(x)    #flatten x
-        y = np.ravel(y)
+        y = np.ravel(y)    #flatten y
 
     N = len(x)
-    l = int((n+1)*(n+2)/2)		# Number of elements in beta
+    l = int((n+1)*(n+2)/2)		#number of elements in beta
     X = np.ones((N,l))
 
     for i in range(1,n+1):
@@ -33,78 +124,282 @@ def create_X(x, y, n):
     return X
 
 
-def OLS(X, z):
+
+
+def features(matrix,degree):
     """
-    Takes in a design matrix and actual data and returning
-    an array of best beta for X and z
+    Slices design matrix
+
+    Args:
+        matrix (ndarray): design matrix
+        degree (int): degree of polynomial
+
+    Returns:
+        X (ndarray): design matrix for the given degree of polynomial
+
     """
-    A = np.linalg.pinv(X.T @ X)
-    beta = A @ X.T @ z
-    return beta
-
-
-def mean_scaler(Xtrain,Xtest,ztrain,ztest):
-    """
-    Takes in design matrix and dataset and returns test and train data
-    scaled by subtracting the mean
-    """
-    X_mean = np.mean(Xtrain, axis=0)
-    X_train = Xtrain - X_mean
-    X_test = Xtest - X_mean
-
-    z_mean = np.mean(ztrain, axis=0)
-    z_train = ztrain - z_mean
-    z_test = ztest - z_mean
-
-    #z_test = z_test - np.mean(z_test, axis=0)#.reshape(int((n+1)*0.8), int((n+1)*0.8))
-    return X_train,X_test,z_train,z_test
-
-
-def features(matrix1,matrix2,degree,):
     l = int((degree + 1) * (degree + 2) / 2)
 
-    matrix1 = matrix1[:,:l]
-    matrix2 = matrix2[:,:l]
+    return matrix[:,:l]
 
 
-    return matrix1,matrix2
 
 
-def MSE(y,ypred):
-    return np.mean((y-ypred)**2)
+def centering(Xtrain,Xtest,ztrain,ztest):
+    """
+    Zero centers design matrix and target values with training mean
 
-def R2(y,ypred):
-    return 1 - np.sum((y - ypred)**2) / np.sum((y - np.mean(y)) ** 2)
+    Args:
+        Xtrain (ndarray): predictor training data
+        Xtest (ndarray): predictor test data
+        ztrain (ndarray): target training data
+        ztrain (ndarray): target test data
+
+    Returns:
+        Arrays (ndarray): scaled training and test data
+    """
+    X_scaler = np.mean(Xtrain, axis=0)
+    X_train_scaled = Xtrain - X_scaler
+    X_test_scaled = Xtest - X_scaler
+
+    z_scaler = np.mean(ztrain, axis=0)
+    z_train_scaled = ztrain - z_scaler
+    z_test_scaled = ztest - z_scaler
+
+    return X_train_scaled,X_test_scaled,z_train_scaled,z_test_scaled
 
 
-def bootstrap_OLS_test(Xtrain,Xtest,ztrain,ztest,nbootstraps):
-    z_pred = np.empty((len(ztest), nbootstraps))
-    for i in range(nbootstraps):
-        X_, z_ = resample(Xtrain, ztrain)
-        beta = OLS(X_, z_)
-        z_pred[:, i] = (Xtest@beta).ravel()
 
-    return z_pred
 
-def bootstrap_OLS(Xtrain,Xtest,ztrain,ztest,nbootstraps,command): #command: either test or train
+def OLS(X, z):
+    """
+    Calculates beta-parameters using ordinary least square method
 
-    if command.lower() == "train":
-        z_pred = np.empty((len(ztrain), nbootstraps))
-        for i in range(nbootstraps):
+    Args:
+        X (ndarray): design matrix
+        z (ndarray): target values
+
+    Returns:
+        beta (ndarray): beta-parameters
+
+    """
+    beta = np.linalg.pinv(X.T @ X) @ X.T @ z
+    return beta
+
+def Ridge(X,z,lam):
+
+    """
+    Calculates beta-parameters using Ridge regression
+
+    Args:
+        X (ndarray): design matrix
+        z (ndarray): target values
+        lambda (float): hyperparameter
+
+    Returns:
+        beta (ndarray): beta-parameters
+
+    """
+    beta = np.linalg.pinv(X.T @ X + lam * np.eye(X.shape[1])) @ X.T @ z
+
+    return beta
+
+def Lasso(X,z,lam):
+
+
+    """
+    Creates a lasso regression model
+
+    Args:
+        X (ndarray): design matrix
+        z (ndarray): target values
+        lambda (float): hyperparameter
+
+    Returns:
+        Model : returns lasso regression model
+
+    """
+    RegLasso = linear_model.Lasso(fit_intercept=False, max_iter=100,alpha=lam).fit(X, z)
+
+    return RegLasso
+
+
+
+def MSE(z,zpred):
+    """
+    Calculates the mean square error
+
+    Args:
+        z (ndarray): target data
+        zpred (ndarray): predicted target data
+
+    Returns:
+        Error (float): mean square error of model
+
+    """
+    return np.mean((z-zpred)**2)
+
+
+
+
+def R2(z,zpred):
+    """
+    Calculates the R2-score
+
+    Args:
+        y (ndarray): target data
+        ypred (ndarray): predicted target data
+
+    Returns:
+        Score (float): R2-score of model
+
+    """
+    return 1 - np.sum((z - zpred)**2) / np.sum((z - np.mean(z)) ** 2)
+
+
+
+def bootstrap(Xtrain,Xtest,ztrain,ztest,B,method,lam):
+
+    """
+    Generating several predictions using same model by resampling training data (nonparemtric bootstrap method)
+
+    Args:
+        Xtrain (ndarray): predictor training data
+        Xtest (ndarray): predictor test data
+        ztrain (ndarray): target training data
+        ztrain (ndarray): target test data
+        B (int): number of bootstraps
+        method (string): regression method (OLS, lasso or ridge)
+
+    Returns:
+        zpred (ndarray): matrix with a unique prediction at each column
+
+
+    """
+
+    if method.lower() == "ols":
+
+        zpred = np.empty((len(ztest), B))
+        for i in range(B):
             X_, z_ = resample(Xtrain, ztrain)
             beta = OLS(X_, z_)
-            z_pred[:, i] = (Xtrain@beta).ravel()
+            zpred[:, i] = (Xtest@beta).ravel()
 
-        return z_pred
 
-    elif command.lower() == "test":
-        z_pred = np.empty((len(ztest), nbootstraps))
-        for i in range(nbootstraps):
+    elif method.lower() == "ridge":
+
+        zpred = np.empty((len(ztest), B))
+        for i in range(B):
             X_, z_ = resample(Xtrain, ztrain)
-            beta = OLS(X_, z_)
-            z_pred[:, i] = (Xtest@beta).ravel()
+            beta = Ridge(X_, z_,lam)
+            zpred[:, i] = (Xtest@beta).ravel()
 
-        return z_pred
 
-    else:
-        print("Unvalid command")
+    elif method.lower() == "lasso":
+        zpred = np.empty((len(ztest), B))
+        for i in range(B):
+            X_, z_ = resample(Xtrain, ztrain)
+            model =  Lasso(X_,z_,lam)
+            zpred[:, i] = model.predict(Xtest).ravel()
+
+    return zpred
+
+
+
+
+
+def crossValidation(X,z,k,method,lam=0):
+
+
+    """
+    Calculate mean square error of model using k-fold cross validation
+
+    Args:
+        X (ndarray): design matrix
+        z (ndarray): target data
+        k (int): number of folds
+        method (string): regression method (OLS, lasso or ridge)
+
+    Returns:
+        error (float): mean square error
+        error2 (float): R2-score
+
+    """
+
+    if method.lower() == "ols":
+
+        kfold = KFold(n_splits = k)
+        error = np.zeros(k)
+        errorR2 = np.zeros(k)
+
+        i=0
+        for train_index, test_index in kfold.split(X):
+            Xtrain = X[train_index,:]
+            ztrain = z[train_index]
+
+            Xtest = X[test_index,:]
+            ztest = z[test_index]
+
+            Xtrain_scaled,Xtest_scaled,ztrain_scaled,ztest_scaled = centering(Xtrain,Xtest,ztrain,ztest)
+
+
+            beta = OLS(Xtrain_scaled, ztrain_scaled)
+            zpred = (Xtest_scaled@beta).ravel()
+
+            error[i] = MSE(ztest_scaled.ravel(),zpred)
+            errorR2[i] = R2(ztest_scaled.ravel(),zpred)
+            i += 1
+
+
+
+
+    elif method.lower() == "ridge":
+
+        kfold = KFold(n_splits = k)
+        error = np.zeros(k)
+        errorR2 = np.zeros(k)
+
+        i=0
+        for train_index, test_index in kfold.split(X):
+            Xtrain = X[train_index,:]
+            ztrain = z[train_index]
+
+            Xtest = X[test_index,:]
+            ztest = z[test_index]
+
+            Xtrain_scaled,Xtest_scaled,ztrain_scaled,ztest_scaled = centering(Xtrain,Xtest,ztrain,ztest)
+
+            beta = Ridge(Xtrain_scaled, ztrain_scaled,lam=lam)
+            zpred = (Xtest_scaled@beta).ravel()
+
+            error[i] = MSE(ztest_scaled.ravel(),zpred)
+            errorR2[i] = R2(ztest_scaled.ravel(),zpred)
+            i += 1
+
+
+    elif method.lower() == "lasso":
+
+        kfold = KFold(n_splits = k)
+        error = np.zeros(k)
+        errorR2 = np.zeros(k)
+
+        i=0
+        for train_index, test_index in kfold.split(X):
+            Xtrain = X[train_index,:]
+            ztrain = z[train_index]
+
+            Xtest = X[test_index,:]
+            ztest = z[test_index]
+
+            Xtrain_scaled,Xtest_scaled,ztrain_scaled,ztest_scaled = centering(Xtrain,Xtest,ztrain,ztest)
+
+
+            model = Lasso(X,z,lam=lam)
+            zpred = model.predict(Xtest_scaled)
+
+            error[i] = MSE(ztest_scaled.ravel(),zpred)
+            errorR2[i] = R2(ztest_scaled.ravel(),zpred)
+            i += 1
+
+
+    return np.mean(error[1:-1]),np.mean(errorR2[1:-1])
